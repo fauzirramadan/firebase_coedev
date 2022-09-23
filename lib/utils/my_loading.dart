440224ceaa5d0df2f6c344a17328aefc97ac3505
fauzirramadan <fauzirramadan@gmail.com>
2022-09-23 13:28:01 +0700

    loading view

---
+++ lib/utils/my_loading.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoadingView extends StatefulWidget {
+  const LoadingView({super.key});
+
+  @override
+  State<LoadingView> createState() => _LoadingViewState();
+}
+
+class _LoadingViewState extends State<LoadingView> {
+  @override
+  Widget build(BuildContext context) {
+    return const CircularProgressIndicator(
+      color: Colors.yellow,
+    );
+  }
+}
